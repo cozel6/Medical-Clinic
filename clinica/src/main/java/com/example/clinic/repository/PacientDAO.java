f83ce5ed171c37eb267960952fd5024d9bf21da5
Cozel <Cozel@DESKTOP-ETQ7VGK>
2025-04-26 12:05:04 +0300

    feat: refactor PacientDAO to extend GenericDAO and include EntityManager

--- clinica/src/main/java/com/example/clinic/repository/PacientDAO.java
+++ clinica/src/main/java/com/example/clinic/repository/PacientDAO.java
@@ -1,5 +1,9 @@
 package com.example.clinic.repository;
 
-public class PacientDAO {
+import com.example.clinic.model.entitati.Pacient;
 
+import jakarta.persistence.EntityManager;
+
+public class PacientDAO extends GenericDAO<Pacient, Long> {
+    public PacientDAO(EntityManager em) { super(Pacient.class, em); }
 }
