b953df751283e16dfaf2d839095e66d19cdc8795
Cozel <Cozel@DESKTOP-ETQ7VGK>
2025-04-26 12:19:39 +0300

    feat: update doctor and patient interfaces with method definitions

--- clinica/src/main/java/com/example/clinic/model/interfete/IActiuniPacient.java
+++ clinica/src/main/java/com/example/clinic/model/interfete/IActiuniPacient.java
@@ -1,6 +1,11 @@
 package com.example.clinic.model.interfete;
 
-public interface  IActiuniPacient {
+import java.util.List;
 
+import com.example.clinic.model.entitati.Programare;
+
+public interface  IActiuniPacient {
+    Programare solicitaProgramare(Programare p);
+    List<Programare> vizualizeazaIstoric();
     }
 
